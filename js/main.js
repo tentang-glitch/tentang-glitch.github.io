6242861ee0e1bfc4bfdc62e0ec34bef435444e04
tentang-glitch <tentangdental@gmail.com>
2025-12-18 10:45:16 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -42,26 +42,29 @@
 
 	var carousel = function() {
 		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
+		  loop: true,
+		  autoplay: true,
+		  margin: 0,
+		  animateOut: 'fadeOut',
+		  animateIn: 'fadeIn',
+		  nav: false,
+		  autoplayHoverPause: false,
+		  items: 1,
+		
+		  // 🔑 FIX SCROLL MOBILE
+		  touchDrag: false,
+		  mouseDrag: false,
+		  pullDrag: false,
+		
+		  navText : [
+		    "<span class='ion-md-arrow-back'></span>",
+		    "<span class='ion-chevron-right'></span>"
+		  ],
+		  responsive:{
+		    0:{ items:1 },
+		    600:{ items:1 },
+		    1000:{ items:1 }
+		  }
 		});
 		$('.carousel-testimony').owlCarousel({
 			autoplay: true,
